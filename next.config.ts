8ce8016933e6b01d612279e57f7ec017b0f4f631
juliusmarminge <julius0216@outlook.com>
2024-10-25 00:27:02 +0200

    repro

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
-import type { NextConfig } from "next";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import nextMdx from "@next/mdx";
+// @ts-expect-error - no types
+import { remarkRehypeWrap } from "remark-rehype-wrap";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+const withMdx = nextMdx({
+  extension: /\.mdx?$/,
+  options: {
+    rehypePlugins: [
+      [
+        remarkRehypeWrap,
+        {
+          node: { type: "element", tagName: "article" },
+          start: "element[tagName=hr]",
+          transform: (article: { children: any[]; properties: any }) => {
+            article.children.splice(0, 1);
+            const heading = article.children.find((n) => n.tagName === "h2");
+            article.properties = {
+              ...heading.properties,
+              title: String(heading),
+            };
+            heading.properties = {};
+            return article;
+          },
+        },
+      ],
+    ],
+  },
+});
+
+
+export default withMdx({
+  pageExtensions: ["ts", "tsx", "mdx"],
+})
